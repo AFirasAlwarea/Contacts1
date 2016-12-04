df946faeb0022a4e8175512ba21ace86298f11be
Firas <Firas@FIRAS-HP>
2016-12-04 22:49:06 +0100

    adding edit and details but not programmed yet

--- Contacts/Content/AngularJS/myApp.js
+++ Contacts/Content/AngularJS/myApp.js
@@ -1,14 +1,14 @@
-﻿angular.module('myApp', []);
+﻿var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
     $scope.FirstName = "Khaled";
     $scope.LastName = "Alwarea";
 
-    $http.get("Home/AjaxThatReturnsJson")
+    $http.get("/Home/AjaxThatReturnsJson")
         .then(function (response) {
             $scope.myJson = response.data;
         });
 
-    $http.get("Home/AjaxThatReturnsJsonList")
+    $http.get("/Home/AjaxThatReturnsJsonList")
         .then(function (response) {
             $scope.myJsonList = response.data;
         });
--- Contacts/Migrations/Configuration.cs
+++ Contacts/Migrations/Configuration.cs
@@ -11,6 +11,7 @@ namespace Contacts.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(Contacts.Models.PeopleDB context)
